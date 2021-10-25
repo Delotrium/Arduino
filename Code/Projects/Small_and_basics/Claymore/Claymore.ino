928650110704192a3773add1e40f5fd171e67a8b
Christian Haller <75713652+Delotrium@users.noreply.github.com>
2021-10-26 09:51:54 +1000

    Added Comments

--- Code/Projects/Small_and_basics/Claymore/Claymore.ino
+++ Code/Projects/Small_and_basics/Claymore/Claymore.ino
@@ -5,33 +5,33 @@
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-int i = 9;
+int i = 9; 
 String nums[] = {"1","2","3","4","5","6","7","8","9"};
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  Serial.begin(9600);
+  Serial.begin(9600); //Setup Serial for debug
   Serial.write("Initialised");
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); //Initialise pin 13 to be output
 }
 
 void loop() {
-  while (i > 0) {
-    lcd.setCursor(0, 0);
-  	lcd.print(nums[i-1]);
-	Serial.write(i);
-    delay(1000);
-    i--;
+  while (i > 0) { //While i is less than 0
+    lcd.setCursor(0, 0); //Select the top right cursor
+  	lcd.print(nums[i-1]); //Print assosiated number from nums[] (note -1 as lists start from [0])
+	Serial.write(i); //Print associated number on LCD
+    delay(1000); //Wait 1 Second
+    i--; //Subtract 1 from i each iteration
   }
-  if (i == 0) {
+  if (i == 0) { //When i reaches 0
     lcd.setCursor(0, 0);
-  	lcd.print("BOOM!");
-    PinStrobe(13);
+  	lcd.print("BOOM!"); //Print "BOOM!" from LCD
+    PinStrobe(13); //Strobe the LED connected to Pin 13
   }
 }
 
-void PinStrobe(int pin)
+void PinStrobe(int pin) //Creates stobe effect for LED on pin 'pin'
 {
   digitalWrite(pin, HIGH);
   delay(100);
